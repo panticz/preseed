2d09dde20c409009439d3f9385c23a5fba5512aa
panticz <mail@konczalski.de>
2014-02-13 12:22:31 +0100

    Add secundary MAC recognition

--- late_command.sh
+++ late_command.sh
@@ -11,6 +11,10 @@ if [ ! -z $1 ]; then
     MAC=$1
 else
     MAC=$(ip link | sed -n "/BROADCAST.*state UP/{n;p}" | tail -1 | tr -s " " | cut -d" " -f3)
+    if [ -z ${MAC} ]; then
+        IFACE=$(route | grep default | sed -e's/  */ /g' | cut -d" " -f8)
+        MAC=$(ip link | sed -n "/${IFACE}/{n;p}" | tail -1 | tr -s " " | cut -d" " -f3)
+    fi
 fi
 
 MAC_HASH=$(echo ${MAC} | md5sum | cut -d" " -f1)
