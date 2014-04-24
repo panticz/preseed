31026f4d9b82f3769c98c4620d00397cc7f462d8
panticz <mail@konczalski.de>
2014-04-24 16:43:28 +0200

    escape variable

--- ipxe/scripts/install_grub_ipxe.sh
+++ ipxe/scripts/install_grub_ipxe.sh
@@ -12,7 +12,7 @@ wget http://dl.panticz.de/ipxe/ipxe.lkrn -O /boot/grub/ipxe.lkrn
 # create GRUB menu entry
 cat <<EOF> /etc/grub.d/49_ipxe 
 #!/bin/sh
-exec tail -n +3 $0
+exec tail -n +3 \$0
 
 menuentry "Network boot (iPXE)" {
   linux16 /boot/grub/ipxe.lkrn
