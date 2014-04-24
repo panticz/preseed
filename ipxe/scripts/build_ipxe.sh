c05b852cc4f6f2f80b397b7970758325196310b7
panticz <mail@konczalski.de>
2014-04-24 12:50:26 +0200

    add build for GRUB image

--- ipxe/scripts/build_ipxe.sh
+++ ipxe/scripts/build_ipxe.sh
@@ -28,5 +28,8 @@ make bin/ipxe.usb EMBED=boot.ipxe
 # build PXE image (/tmp/ipxe/src/bin/ipxe.pxe)
 make bin/ipxe.pxe EMBED=boot.ipxe
  
+# build GRUB image (/tmp/ipxe/src/bin/ipxe.lkrn)
+make bin/ipxe.lkrn EMBED=boot.ipxe
+ 
 # build undionly image (/tmp/ipxe/src/bin/undionly.kpxe)
 make bin/undionly.kpxe EMBED=boot.ipxe
