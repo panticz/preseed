13b99d73a1dc6bf4d37b4b4230cb08be51f21ef1
panticz <mail@konczalski.de>
2014-08-21 13:15:35 +0200

    use enable_auto_update.sh script

--- late_command.sh
+++ late_command.sh
@@ -665,11 +665,7 @@ function addToFuse() {
 }
 
 function enable_auto_update() {
-    sed -i '4s|//|  |g' /etc/apt/apt.conf.d/50unattended-upgrades
-    sed -i 's|//Unattended-Upgrade::Mail|Unattended-Upgrade::Mail|g' /etc/apt/apt.conf.d/50unattended-upgrades
-    sed -i 's|APT::Periodic::Download-Upgradeable-Packages "0";|APT::Periodic::Download-Upgradeable-Packages "1";|g' /etc/apt/apt.conf.d/10periodic
-    sed -i 's|APT::Periodic::AutocleanInterval "0";|APT::Periodic::AutocleanInterval "1";|g' /etc/apt/apt.conf.d/10periodic
-    echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/10periodic
+    wget --no-check-certificate https://raw.githubusercontent.com/panticz/scripts/master/enable_auto_update.sh -O - | bash -
 }
 
 function install_ipmitool() {
