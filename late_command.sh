abc9dc05648b4b797c1554060fe4c123886c15a7
panticz <mail@konczalski.de>
2014-09-18 11:00:36 +0200

    customize gedit on pakonb

--- late_command.sh
+++ late_command.sh
@@ -655,6 +655,10 @@ gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 gsettings set org.gnome.nautilus.list-view default-zoom-level 'smallest'
 
 # configure gedit
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+gsettings set org.gnome.gedit.preferences.editor bracket-matching true
 gsettings set org.gnome.gedit.preferences.editor create-backup-copy false
 
 # restore OpenVPN configuration
