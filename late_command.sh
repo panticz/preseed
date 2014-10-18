66a68ab6b324c5b12fe4fa0d8824c8f927825fe5
panticz <mail@konczalski.de>
2014-10-18 12:22:22 +0200

    Update UNO packages to LibreOffice

--- late_command.sh
+++ late_command.sh
@@ -388,7 +388,7 @@ function install_develop() {
 # install mr
 function install_mr() {
     install -y openvpn network-manager-openvpn tofrodos imagemagick sshfs patch
-    apt-get install -y openoffice.org-java-common openoffice.org-base openoffice.org-officebean
+    apt-get install -y libreoffice-java-common libreoffice-base libreoffice-officebean
 #err?    install_yajhfc
     install_epson_business_inkjet 
     install_HP-Laserjet-4050n HP-Laserjet-4050n 192.168.1.18
