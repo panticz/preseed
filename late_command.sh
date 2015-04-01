44d8cf25c9c324114d6e660976916bac30f62567
panticz <mail@konczalski.de>
2015-04-01 12:32:44 +0200

    update to new mac

--- late_command.sh
+++ late_command.sh
@@ -601,7 +601,7 @@ function configure_pakonb() {
 sudo apt-get clean | tee -a ${LOG}
 
 # preconfigure fstab
-if [ "${MAC_HASH}" == "ca9ee1b29e06b343d73346b82a717e3d" ]; then
+if [ "${MAC_HASH}" == "6d3df838e151801d54417b65002d923a" ]; then
 #    echo "/dev/mapper/vg0-data    /home                 ext4    defaults      0  2" >> /etc/fstab
     echo "#/dev/sda4    /home                 ext4    defaults      0  2" >> /etc/fstab
 fi
