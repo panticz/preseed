deb6327129fa1539af6f88fb382cd4feee6a384e
panticz <mail@konczalski.de>
2016-07-07 16:44:55 +0200

    add support for /boot/grub2/ path

--- ipxe/scripts/install_grub_ipxe.sh
+++ ipxe/scripts/install_grub_ipxe.sh
@@ -7,7 +7,7 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # download GRUB iPXE boot image
-wget -q http://dl.panticz.de/ipxe/ipxe.lkrn -O /boot/grub/ipxe.lkrn
+wget -q http://dl.panticz.de/ipxe/ipxe.lkrn -O "$(ls -d /boot/grub?)/ipxe.lkrn"
 
 # create GRUB menu entry
 wget -q https://raw.githubusercontent.com/panticz/preseed/master/ipxe/scripts/grub/49_ipxe -O /etc/grub.d/49_ipxe 
